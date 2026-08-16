0e4159992db939f6a463f844a02aafb8e95c444a
a48161845-jpg <a48161845@gmail.com>
2026-08-16 16:20:23 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,21 +8,21 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 USER_ID = 7958252339
+ROSE_ID = "5168103777563050263"
+
 
 async def main():
     bot = Bot(BOT_TOKEN)
 
-    gifts = await bot.get_available_gifts()
-
-    print("Доступные подарки:")
-
-    for gift in gifts.gifts:
-        print(
-            f"ID: {gift.id} | "
-            f"Stars: {gift.star_count}"
+    try:
+        await bot.send_gift(
+            user_id=USER_ID,
+            gift_id=ROSE_ID,
+            text="🌹 Роза от бота!"
         )
-
-    await bot.session.close()
+        print("✅ Роза отправлена!")
+    finally:
+        await bot.session.close()
 
 
 asyncio.run(main())
